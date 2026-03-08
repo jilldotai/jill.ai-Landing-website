b31f457cbf2fb33041421882d311fe2e767dde71
Estelle Coetzee <jilldotai@gmail.com>
2026-03-09 01:26:30 +0200

    Merge branch 'main' of https://github.com/jilldotai/jill.ai-Landing-website

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-<<<<<<< HEAD
-import "./.next/types/routes.d.ts";
-=======
->>>>>>> 1ef9574d953de48238ed3800f4740a5c6e21a9f0
+import "./.next/dev/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
